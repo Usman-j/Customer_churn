80f071195c734b63c92652e12772397194c524b9
Usman <mu.usman.1993@gmail.com>
2022-04-03 04:42:31 +0000

    Completed churn_library

--- .ipynb_checkpoints/churn_library-checkpoint.py
+++ .ipynb_checkpoints/churn_library-checkpoint.py
@@ -1,36 +1,92 @@
 # library doc string
-
+'''
+Library for developig Customer churn prediction model.
+'''
 
 # import libraries
+import shap
+import joblib
+import logging
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns; sns.set()
+from sklearn.preprocessing import normalize
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import plot_roc_curve, classification_report
 
-
-
+logging.basicConfig(
+    filename='./logs/results.log',
+    level=logging.INFO,
+    filemode='w',
+    format='%(name)s - %(levelname)s - %(message)s')
 
 def import_data(pth):
     '''
     returns dataframe for the csv found at pth
 
     input:
-            pth: a path to the csv
+            pth: string path to the csv
     output:
             df: pandas dataframe
     '''	
-	pass
-
+    try:
+        df = pd.read_csv(pth,index=False)
+        logging.info("SUCCESS: {} shape of read dataframe".format(df.shape))
+        return df
+    except FileNotFoundError: 
+        logging.error("ERROR: File not found in the given path")
 
 def perform_eda(df):
     '''
     perform eda on df and save figures to images folder
     input:
             df: pandas dataframe
-
     output:
             None
     '''
-	pass
+    try:
+        path_img_folder = './images/eda/'
+        df['Churn'] = df['Attrition_Flag'].apply(lambda val: 0 if val == "Existing Customer" else 1)
+        plt.figure(figsize=(20,10)) 
+        df['Churn'].hist()
+        plt.savefig(path_img_folder+'Churn_histogram.png')
+        plt.close()
+        logging.info('SUCCESS: Churn histogram figure saved.')
+        
+        plt.figure(figsize=(20,10)) 
+        df['Customer_Age'].hist()
+        plt.savefig(path_img_folder+'Customer_Age_histogram.png')
+        plt.close()
+        logging.info('SUCCESS: Customer_Age histogram figure saved.')
+        
+        plt.figure(figsize=(20,10)) 
+        df.Marital_Status.value_counts('normalize').plot(kind='bar')
+        plt.savefig(path_img_folder+'Marital_status_bar_plot.png')
+        plt.close()
+        logging.info('SUCCESS: Marital_status bar plot figure saved.')
+        
+        plt.figure(figsize=(20,10)) 
+        sns.distplot(df['Total_Trans_Ct']);
+        plt.savefig(path_img_folder+'Total_Trans_Ct_dist_plot.png')
+        plt.close()
+        logging.info('SUCCESS: Total_Trans_Ct distribution plot figure saved.')
+        
+        plt.figure(figsize=(20,10)) 
+        sns.heatmap(df.corr(), annot=False, cmap='Dark2_r', linewidths = 2)
+        plt.savefig(path_img_folder+'Correlation_heatmap.png')
+        plt.close()
+        logging.info('SUCCESS: Correlation heatmap figure saved.')
+        
+    except KeyError as e:
+        logging.error('ERROR: Column {} for eda not found in dataframe'.format(e))
+    
 
 
-def encoder_helper(df, category_lst, response):
+def encoder_helper(df, category_lst, response='Churn'):
     '''
     helper function to turn each categorical column into a new column with
     propotion of churn for each category - associated with cell 15 from the notebook
@@ -38,19 +94,31 @@ def encoder_helper(df, category_lst, response):
     input:
             df: pandas dataframe
             category_lst: list of columns that contain categorical features
-            response: string of response name
+            response: string of response name [optional argument]
 
     output:
-            df: pandas dataframe with new columns for
+            df: pandas dataframe with new columns for each categorical feature encoded with respect to proportion of response
     '''
-    pass
+    try:
+        for feat in category_lst:
+            feat_lst = []
+            feat_groups = df.groupby(feat).mean()[response]
+            for val in df[feat]:
+                feat_lst.append(feat_groups.loc[val])
+            df[feat+'_'+response] = feat_lst
+
+        logging.info('SUCCESS: Categorical features encoded with respect to response proportion and added as new columns.')
+        return df
+    except KeyError as e:
+        logging.error('ERROR: Column {} not found in dataframe for encoding'.format(e))
 
 
-def perform_feature_engineering(df, response):
+def perform_feature_engineering(df, feat_lst, response='Churn'):
     '''
     input:
               df: pandas dataframe
-              response: string of response name
+              feat_lst: list of features to keep for model development
+              response: string of response name [optional argument that could be used for naming variables or index y column]
 
     output:
               X_train: X training data
@@ -58,6 +126,18 @@ def perform_feature_engineering(df, response):
               y_train: y training data
               y_test: y testing data
     '''
+    
+    try:
+        y = df[response]
+        X = pd.DataFrame()
+        X[feat_lst] = df[feat_lst]
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size= 0.3, random_state=42)
+        logging.info('SUCCESS: Data split into train and test.')
+        return X_train, X_test, y_train, y_test
+    
+    except KeyError:
+        logging.error('ERROR: Feature columns not found in dataframe for data splitting.')
+        
 
 def classification_report_image(y_train,
                                 y_test,
@@ -79,7 +159,25 @@ def classification_report_image(y_train,
     output:
              None
     '''
-    pass
+    plt.rc('figure', figsize=(5, 5))
+    #plt.text(0.01, 0.05, str(model.summary()), {'fontsize': 12}) old approach
+    plt.text(0.01, 1.25, str('Random Forest Train'), {'fontsize': 10}, fontproperties = 'monospace')
+    plt.text(0.01, 0.05, str(classification_report(y_test, y_test_preds_rf)), {'fontsize': 10}, fontproperties = 'monospace') 
+    plt.text(0.01, 0.6, str('Random Forest Test'), {'fontsize': 10}, fontproperties = 'monospace')
+    plt.text(0.01, 0.7, str(classification_report(y_train, y_train_preds_rf)), {'fontsize': 10}, fontproperties = 'monospace')
+    plt.axis('off')
+    plt.grid(False)
+    plt.savefig('./images/results/Random_Forest_Classification_report.png',bbox_inches='tight')
+    plt.close()
+    plt.rc('figure', figsize=(5, 5))
+    plt.text(0.01, 1.25, str('Logistic Regression Train'), {'fontsize': 10}, fontproperties = 'monospace')
+    plt.text(0.01, 0.05, str(classification_report(y_train, y_train_preds_lr)), {'fontsize': 10}, fontproperties = 'monospace')
+    plt.text(0.01, 0.6, str('Logistic Regression Test'), {'fontsize': 10}, fontproperties = 'monospace')
+    plt.text(0.01, 0.7, str(classification_report(y_test, y_test_preds_lr)), {'fontsize': 10}, fontproperties = 'monospace') 
+    plt.axis('off')
+    plt.grid(False)
+    plt.savefig('./images/results/Logistic_Regression_Classification_report.png',bbox_inches='tight')
+    plt.close()
 
 
 def feature_importance_plot(model, X_data, output_pth):
@@ -93,7 +191,29 @@ def feature_importance_plot(model, X_data, output_pth):
     output:
              None
     '''
-    pass
+    # Calculate feature importances
+    importances = model.feature_importances_
+    # Sort feature importances in descending order
+    indices = np.argsort(importances)[::-1]
+
+    # Rearrange feature names so they match the sorted feature importances
+    names = [X_data.columns[i] for i in indices]
+
+    # Create plot
+    plt.figure(figsize=(20,5))
+
+    # Create plot title
+    plt.title("Feature Importance")
+    plt.ylabel('Importance')
+
+    # Add bars
+    plt.bar(range(X_data.shape[1]), importances[indices])
+
+    # Add feature names as x-axis labels
+    plt.xticks(range(X_data.shape[1]), names, rotation=90)
+    plt.savefig(output_pth+'RandomForest_FeatureImportance.png',bbox_inches='tight')
+    plt.close()
+    
 
 def train_models(X_train, X_test, y_train, y_test):
     '''
@@ -106,4 +226,49 @@ def train_models(X_train, X_test, y_train, y_test):
     output:
               None
     '''
-    pass
+    # grid search
+    rfc = RandomForestClassifier(random_state=42)
+    lrc = LogisticRegression()
+
+    param_grid = { 
+        'n_estimators': [200, 500],
+        'max_features': ['auto', 'sqrt'],
+        'max_depth' : [4,5,100],
+        'criterion' :['gini', 'entropy']
+    }
+
+    cv_rfc = GridSearchCV(estimator=rfc, param_grid=param_grid, cv=5)
+    cv_rfc.fit(X_train, y_train)
+    
+    lrc.fit(X_train, y_train)
+    
+    y_train_preds_rf = cv_rfc.best_estimator_.predict(X_train)
+    y_test_preds_rf = cv_rfc.best_estimator_.predict(X_test)
+    
+    # save best model
+    joblib.dump(cv_rfc.best_estimator_, './models/rfc_model.pkl')
+    joblib.dump(lrc, './models/logistic_model.pkl')
+    
+    y_train_preds_lr = lrc.predict(X_train)
+    y_test_preds_lr = lrc.predict(X_test)
+    
+    # Saving classification report
+    classification_report_image(y_train,
+                                y_test,
+                                y_train_preds_lr,
+                                y_train_preds_rf,
+                                y_test_preds_lr,
+                                y_test_preds_rf)
+    
+    # Saving ROC curves
+    lrc_plot = plot_roc_curve(lrc, X_test, y_test)
+    plt.figure(figsize=(15, 8))
+    ax = plt.gca()
+    rfc_disp = plot_roc_curve(cv_rfc.best_estimator_, X_test, y_test, ax=ax, alpha=0.8)
+    lrc_plot.plot(ax=ax, alpha=0.8)
+    plt.savefig('./images/results/ROC_curves.png',bbox_inches='tight')
+    plt.close()
+    
+    # Saving Feature importance for RF model
+    rfc_model = joblib.load('./models/rfc_model.pkl')
+    feature_importance_plot(rfc_model, X_train, './images/results/')
